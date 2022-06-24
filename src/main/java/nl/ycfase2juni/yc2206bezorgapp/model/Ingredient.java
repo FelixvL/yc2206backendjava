a5de5f5e4b10ca4339759769896fec679e195dc4
felixvl <digitaleservice@gmail.com>
2022-06-24 11:22:13 +0200

    tomany relatie gemaakt ingredient

--- src/main/java/nl/ycfase2juni/yc2206bezorgapp/model/Ingredient.java
+++ src/main/java/nl/ycfase2juni/yc2206bezorgapp/model/Ingredient.java
@@ -1,40 +1,43 @@
 package nl.ycfase2juni.yc2206bezorgapp.model;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+
 
 @Entity
-public class Maaltijd {
+public class Ingredient {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+	long id;
 	
-	private String naam;
-
-	@ManyToMany
-	private List<Ingredient> ingredient1;
-	
+	String naam;
+	String allergeen;
+	int kcal;
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
 	public String getNaam() {
 		return naam;
 	}
-
 	public void setNaam(String naam) {
 		this.naam = naam;
+	}
+	public String getAllergeen() {
+		return allergeen;
+	}
+	public void setAllergeen(String allergeen) {
+		this.allergeen = allergeen;
+	}
+	public int getKcal() {
+		return kcal;
+	}
+	public void setKcal(int kcal) {
+		this.kcal = kcal;
 	}
 	
 	
