16a1c1327138450a5fcf33eec55cceb782429e9c
98Suzanne <s.dinten@upcmail.nl>
2022-07-26 12:33:01 +0200

    delete knoppen voor klant en bezorger aangemaakt

--- src/main/java/nl/ycfase2juni/yc2206bezorgapp/restcontroller/KlantEndpoint.java
+++ src/main/java/nl/ycfase2juni/yc2206bezorgapp/restcontroller/KlantEndpoint.java
@@ -3,23 +3,27 @@ package nl.ycfase2juni.yc2206bezorgapp.restcontroller;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import nl.ycfase2juni.yc2206bezorgapp.dto.BestellingDto;
 import nl.ycfase2juni.yc2206bezorgapp.dto.KlantDto;
 import nl.ycfase2juni.yc2206bezorgapp.model.Klant;
-import nl.ycfase2juni.yc2206bezorgapp.model.Restaurant;
 import nl.ycfase2juni.yc2206bezorgapp.persistence.KlantService;
+import nl.ycfase2juni.yc2206bezorgapp.persistence.KlantRepository;
 
 @RestController
 public class KlantEndpoint {
 	
 	@Autowired
 	KlantService ks;
-
+	
+	@Autowired
+	KlantRepository kr;
+	
 	@GetMapping("overzichtklanten")
 	public Stream<KlantDto> overzichtKlanten() {
 		return ks.geefAlleKlanten().stream().map(KlantDto::createFromModel);
@@ -30,6 +34,12 @@ public class KlantEndpoint {
 		System.out.println(k.getNaam());
 		ks.klantInvoeren(k);
 		
+	}
+	
+	@DeleteMapping("verwijderklant/{klantid}")
+	public void verwijderKlant(@PathVariable("klantid") int klantid){
+		long kid = klantid;
+		kr.deleteById(kid);
 	}
 	
 }
